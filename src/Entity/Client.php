6c479aa4a3039049f00654d187d41776522037d0
Bob Ross <nicolas.hallaert.pro@gmail.com>
2019-01-15 14:55:27 +0100

    Added script for table color change; CSS feature for edit / show and index of Clients; Fixed issue with return button;

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use App\Repository\ClientRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -65,6 +66,11 @@ class Client
         $this->lastname = $lastname;
 
         return $this;
+    }
+
+    public function getNbofbooks()
+    {
+        return count($this->getBooks());
     }
 
     /**
